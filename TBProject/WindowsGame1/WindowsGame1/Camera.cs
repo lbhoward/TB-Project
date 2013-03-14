3e3445db8a47f1a472f8709cd930deccbf47668d
Lawrence Bradley Howard <lawrence_howard@inbox.com>
2013-03-14 19:15:19 +0000

    Skeleton Build
    Blank XNA project with included Camera Class and 3D-Drawing functions.

---
+++ TBProject/WindowsGame1/WindowsGame1/Camera.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TBProject
+{
+    class Camera
+    {
+        public Vector3 LookAt
+        {
+            get { return lookAt; }
+            set { lookAt = value; }
+        }
+        private Vector3 lookAt;
+
+        public Vector3 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+        private Vector3 position;
+
+        public bool CockpitView = false;
+        public float FOV = 45.0f;
+
+        public Matrix Projection
+        {
+            get { return projection; }
+        }
+        private Matrix projection;
+
+        public Matrix View
+        {
+            get { return view; }
+        }
+        private Matrix view;
+
+        public Camera(Vector3 POSITION, Vector3 LOOKAT, float RATIO)
+        {
+            view = Matrix.CreateLookAt(POSITION, LOOKAT, Vector3.Up);
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(FOV), RATIO, 0.01f, 50.0f);
+        }
+
+        public void Update()
+        {
+            view = Matrix.CreateLookAt(position, lookAt, Vector3.Up);
+        }
+    }
+}
---
+++ TBProject/WindowsGame1/WindowsGame1/Game1.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TBProject
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Camera camera;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            //Initialise the camera
+            //new Camera(Position(GLOBAL),LookAt(GLOBAL),AspectRatio(W/H));
+            camera = new Camera(new Vector3(0,0,0), new Vector3(0,0,0),
+                (float)graphics.GraphicsDevice.Viewport.Width / graphics.GraphicsDevice.Viewport.Height);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        //3D Model Draw Function
+        //DrawModel(Model, MatrixBelongingToModel)
+        private void DrawModel(Model model, Matrix world)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.World = transforms[mesh.ParentBone.Index] * world;
+                    effect.View = camera.View;
+                    effect.Projection = camera.Projection;
+                }
+                mesh.Draw();
+            }
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
